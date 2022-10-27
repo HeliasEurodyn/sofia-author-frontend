5e91b11f7d3a87153a1d45f6deb09a712e25ac24
HeliasEurodyn <helias.karagozidis@eurodyn.com>
2022-10-27 10:12:28 +0300

    SSE Implementations

--- src/app/services/crud/sse-notification.service.ts
+++ src/app/services/crud/sse-notification.service.ts
@@ -11,9 +11,21 @@ import {SseNotificationResponseDTO} from '../../dtos/sse-notification/sse-notifi
 })
 export class SseNotificationService extends CrudService<any> {
   private eventSource: EventSourcePolyfill = null;
+  private eventListenerFunction = null;
+  private topic = null;
 
   constructor(public http: HttpClient) {
     super(http, 'sse-notification');
+    this.startConnectionStatusTimer();
+  }
+
+  private startConnectionStatusTimer() {
+    setInterval(() => {
+      if(this.eventSource == null) return;
+      if(this.eventListenerFunction == null) return;
+      if(this.topic == null) return;
+      if(this.eventSource.readyState === 2) this.connect();
+    },20000);
   }
 
   sendNotification(sseNotificationResponseDTO: SseNotificationResponseDTO): Observable<any> {
@@ -29,28 +41,33 @@ export class SseNotificationService extends CrudService<any> {
     this.eventSource.close();
   }
 
-  subscribe(topic, eventListenerFunction: (event) => void): EventSourcePolyfill {
+  public subscribe(topic, eventListenerFunction: (event) => void) {
+   this.topic = topic;
+   this.eventListenerFunction = eventListenerFunction;
+   this.connect();
+  }
+
+  private connect(){
     this.eventSource = new EventSourcePolyfill(`${environment.serverUrl}/${this.endpoint}/subscribe`, {
       headers: {
         'Authorization': 'Bearer ' + localStorage.getItem('jwt_token')
       }
-    })
+    });
 
     this.eventSource.onopen = () => {
-     console.log('open');
+      console.log('open');
     }
 
     this.eventSource.addEventListener('keepAlive', (ev: MessageEvent) => {
       console.log(ev.data);
     }, false);
 
-    this.eventSource.addEventListener(topic, eventListenerFunction, false);
+    this.eventSource.addEventListener(this.topic, this.eventListenerFunction, false);
+
     this.eventSource.onerror = () => {
       this.eventSource.close();
-      setTimeout(() => this.eventSource = this.subscribe(topic, eventListenerFunction),10000);
     };
 
-    return this.eventSource;
   }
 
 }
