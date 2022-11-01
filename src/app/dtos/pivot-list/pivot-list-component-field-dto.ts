e9727f3def0c3cf269e79ba07fd05e696f15e1b4
christos.aslamatzidis <christos.aslamatzidis@eurodyn.com>
2022-11-01 17:53:11 +0200

    Remove unnecessary dto

--- src/app/dtos/pivot-list/pivot-list-component-field-dto.ts
+++
@@ -1,8 +0,0 @@
-import {ListComponentFieldDTO} from '../list/list-component-field-d-t-o';
-import {FilterField} from './filter-field';
-
-export class PivotListComponentFieldDTO extends ListComponentFieldDTO {
-  public filterFields: FilterField[] = [];
-  public isFullChecked = true;
-  public isExpanded = true;
-}
