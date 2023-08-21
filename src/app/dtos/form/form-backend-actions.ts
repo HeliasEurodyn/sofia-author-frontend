2feee369252608d07863ddce082c1309167e876b
KonstatninosAftzis <konstantinos.aftzis@eurodyn.com>
2023-08-21 17:56:47 +0300

    Backend Actions Implementation

--- src/app/dtos/form/form-backend-actions.ts
+++ src/app/dtos/form/form-backend-actions.ts
@@ -2,6 +2,7 @@ import { BaseDTO } from "../common/base-dto";
 
 export class FormBackendActions extends BaseDTO {
 
+    public title: string;
     public editor: string;
     public trigger_on: string;
 }
--- src/app/dtos/rest-documentation/rest-documentation-endpoint/exclude-endpoint-field-dto.ts
+++ src/app/dtos/rest-documentation/rest-documentation-endpoint/exclude-endpoint-field-dto.ts
@@ -1,10 +1,10 @@
-import { BaseDTO } from "../../common/base-dto";
+import { BaseDTO } from '../../common/base-dto';
 
 export class ExcludeEndpointFieldDTO extends BaseDTO {
+
+   public  excludeField: string;
    trim() {
      throw new Error('Method not implemented.');
    }
-
-   public  excludeField: string;
 
 }
